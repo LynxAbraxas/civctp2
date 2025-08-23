ffb6cbb4bfdb94fc4324cc3d8b5925abea2631c8
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2025-08-23 23:26:27 +0200

    TMP: use terrain.txt to determin advance enabling specific terraform (not working yet)

--- tools/advBenefits.sh
+++ tools/advBenefits.sh
@@ -3,9 +3,10 @@
 for f in ../ctp2_data/default/gamedata/Units.txt \
 	     ../ctp2_data/default/gamedata/buildings.txt \
 	     ../ctp2_data/default/gamedata/Wonder.txt \
-	     ../ctp2_data/default/gamedata/tileimp.txt
+	     ../ctp2_data/default/gamedata/tileimp.txt \
+	     ../ctp2_data/default/gamedata/terrain.txt
 	     do
-	 awk '/{/ && !/TerrainEffect/ {a=$1; t=0}; /TerrainEffect.*{/ {t++}; $1 ~ /^EnableAdvance$/ && t<2 {print $2, a}' $f \
+	 awk '/{/ && !/TerrainEffect/ {a=$1; t=0}; /TerrainEffect.*{/ {t++}; $1 ~ /^EnableAdvance$/ || $1 ~ /^AddAdvance$/ && t<2 {print $2, a}' $f \
 	     | sort \
 	     | awk '{arr[$1] = arr[$1] "</TD></TR><TR><TD>" $2} END {for (i in arr) print i, arr[i]}' \
 	     | awk -v file=$1 -f ../tools/advBenefits.awk
